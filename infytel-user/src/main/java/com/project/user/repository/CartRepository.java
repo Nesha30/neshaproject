1f4f410a3520b51bc9165b7b1171e89da4a544d7
Nesha30 <108213668+Nesha30@users.noreply.github.com>
2022-08-06 23:40:09 +0530

    Delete infytel-user directory

--- infytel-user/src/main/java/com/project/user/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.user.entity.cart;
-
-public interface CartRepository extends JpaRepository <cart, Integer> {}
-
-
